a40b37d30a89b8d577889e32794b686802268690
saitejach127 <saitejach127@gmail.com>
2020-11-26 20:43:19 +0530

    Testing the action

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const core = require("@actions/core");
 
 try {
     console.log(github.context.payload);
+    console.log("\n\n\n");
+    console.log(process.env.GITHUB_REF);
 } catch (e) {
     core.setFailed(e.message);
 }
